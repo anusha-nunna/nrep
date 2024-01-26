57ee216d104712ffb091a88fb78b30023aa16a5a
anusha-nunna <116234651+anusha-nunna@users.noreply.github.com>
2024-01-26 15:57:01 -0600

    Add files via upload

---
+++ NAME MANGLING_Accessspecifiers.py
@@ -0,0 +1,30 @@
+# NAME MANGLING
+#1st way
+class Class:
+    def __init__(self):
+        self.name = "anusha"
+        
+    def display(self):
+        self.__age = 24    # age is private
+    
+object_1 = Class()
+
+object_1.display()
+print(object_1._Class__age)
+
+#===============================================================
+# 2nd way
+class Class:
+    def __init__(self):
+        self.name = "anusha"
+        
+    def __display(self):
+        self.__age = 24    # age is private
+        
+    def DisplayPrivateData(self):
+        self.__display()
+        
+object_1 = Class()
+
+object_1.DisplayPrivateData()
+print(object_1._Class__age)
