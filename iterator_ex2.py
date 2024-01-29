e8830a9f77684aebbe898560d1ca3f0a868a01da
anusha-nunna <116234651+anusha-nunna@users.noreply.github.com>
2024-01-29 13:50:24 -0600

    Create iterator_ex2.py

---
+++ iterator_ex2.py
@@ -0,0 +1,19 @@
+class Sentence:
+    def __init__(self,sentence):
+        self.a = sentence
+        self.index = 0  #keep track of the current position while iterating over the words
+        self.b = self.a.split()  #Splits the input sentence into words
+    def __iter__(self):   #make the class iterable
+        return self
+        
+    def __next__(self):   #generating the next item
+        if self.index >= len(self.b):
+            raise StopIteration
+        var = self.index
+        self.index += 1
+        return self.b[var]
+
+object = Sentence("Hello Anusha How are you")
+for word in object:
+    print(word)
+    
