cbbae751fcf9e031b012f5ecdd77373567b4070b
anusha-nunna <116234651+anusha-nunna@users.noreply.github.com>
2024-01-29 12:35:35 -0600

    Update Iterators in python.py

--- Iterators in python.py
+++ Iterators in python.py
@@ -1,6 +1,7 @@
 #coming to for loop when we create the list the values will be stored in memor but iter the values will not stored in the memory. when we use inbuild funtion "next" it will store the memory and print the value
-
-
+#list is iterable if we want to convert from iterable to iterator then we use the iter function 
+#initially the iterator values will not br stored in memory when we call the iter next function then it will stored in memory 
+#when i call next, first value will be will print. when i call nect agian second value will be print
 #The only way to stop the exception for loop is that we need to raise StopIteration and it handles the exception internally 
 
 #Using Iterator
